65867ff8ce234521f12a10690a6657f9ac0b9c3b
Adeesha97 <adeeshakawshal1997@gmail.com>
2020-10-11 20:58:44 +0530

    done

--- navigation/meals-navigator.js
+++ navigation/meals-navigator.js
@@ -15,158 +15,221 @@ import CategoriesScreen from '../screens/CategoriesScreen'
 import CategoriesProductsScreen from '../screens/CategoriesProductsScreen'
 import ProductsDetailScreen from '../screens/ProductsDetailScreen'
 import HomeScreen from '../screens/home-screen'
+import DeliveryDetailsScreen from "../screens/delivery-details-screen";
+import PaymentScreen from "../screens/payment-details-screen";
+import PaymentCardScreen from "../screens/payment-details-screen2";
 
 const HomeNavigator = createStackNavigator(
-  {
-    Categories: {
-      screen: HomeScreen
+    {
+        Categories: {
+            screen: HomeScreen
+        },
+        CategoriesProducts: {
+            screen: CategoriesProductsScreen
+        },
+        ProductDetails: {
+            screen: ProductsDetailScreen
+        }
     },
-    CategoriesProducts: {
-      screen: CategoriesProductsScreen
-    },
-    ProductDetails: {
-      screen: ProductsDetailScreen
+    {
+        defaultNavigationOptions: {
+
+            headerStyle: {
+                backgroundColor: Colors.primaryColor
+            },
+            headerTintColor: Colors.secondaryColor
+        }
     }
-  },
-  {
-    defaultNavigationOptions: {
-      
-      headerStyle: {
-        backgroundColor: Colors.primaryColor
-      },
-      headerTintColor: Colors.secondaryColor
-    }
-  }
 )
 
 const ProfileNavigator = createStackNavigator(
-  {
-    Profile: ProfileScreen
-  },
-  {
-    defaultNavigationOptions: {
-      headerTitleAlign: 'center',
-      headerStyle: {
-        backgroundColor: Colors.primaryColor
-      },
-      headerTintColor: Colors.secondaryColor
+    {
+        Profile: ProfileScreen
+    },
+    {
+        defaultNavigationOptions: {
+            headerTitleAlign: 'center',
+            headerStyle: {
+                backgroundColor: Colors.primaryColor
+            },
+            headerTintColor: Colors.secondaryColor
+        }
     }
-  }
 )
 
 const OrdersNavigator = createStackNavigator(
-  {
-    Orders: OrdersScreen,
-    OrderDetails: OrderDetailsScreen
-  },
-  {
-    defaultNavigationOptions: {
-      headerTitleAlign: 'center',
-      headerStyle: {
-        backgroundColor: Colors.primaryColor
-      },
-      headerTintColor: Colors.secondaryColor
+    {
+        Orders: OrdersScreen,
+        OrderDetails: OrderDetailsScreen
+    },
+    {
+        defaultNavigationOptions: {
+            headerTitleAlign: 'center',
+            headerStyle: {
+                backgroundColor: Colors.primaryColor
+            },
+            headerTintColor: Colors.secondaryColor
+        }
     }
-  }
 )
 
 const FeedbackNavigator = createStackNavigator(
-  {
-    Feedback: FeedbackScreen
-  },
-  {
-    defaultNavigationOptions: {
-      headerTitleAlign: 'center',
-      headerStyle: {
-        backgroundColor: Colors.primaryColor
-      },
-      headerTintColor: Colors.secondaryColor
+    {
+        Feedback: FeedbackScreen
+    },
+    {
+        defaultNavigationOptions: {
+            headerTitleAlign: 'center',
+            headerStyle: {
+                backgroundColor: Colors.primaryColor
+            },
+            headerTintColor: Colors.secondaryColor
+        }
     }
-  }
 )
 
 const CartNavigator = createStackNavigator(
-  {
-    Cart: CartScreen
-  },
-  {
-    defaultNavigationOptions: {
-      headerTitleAlign: 'center',
-      headerStyle: {
-        backgroundColor: Colors.primaryColor
-      },
-      headerTintColor: Colors.secondaryColor
+    {
+        Cart: {
+            screen: CartScreen
+        },
+        DeliveryDetails: {
+            screen: DeliveryDetailsScreen
+        },
+        PaymentDetails: {
+            screen: PaymentScreen
+        },
+        PaymentDetails2: {
+            screen: PaymentCardScreen
+        },
+
+    },
+
+    {
+        defaultNavigationOptions: {
+
+            headerStyle: {
+                backgroundColor: Colors.primaryColor
+            },
+            headerTintColor: Colors.secondaryColor
+        }
     }
-  }
 )
 
+const PaymentNavigator = createStackNavigator(
+    {
+        Cart: PaymentScreen
+    },
+    {
+        defaultNavigationOptions: {
+            headerTitleAlign: 'center',
+            headerStyle: {
+                backgroundColor: Colors.primaryColor
+            },
+            headerTintColor: Colors.secondaryColor
+        }
+    }
+)
+
+const PaymentCardNavigator = createStackNavigator(
+    {
+        Cart: PaymentCardScreen
+    },
+    {
+        defaultNavigationOptions: {
+            headerTitleAlign: 'center',
+            headerStyle: {
+                backgroundColor: Colors.primaryColor
+            },
+            headerTintColor: Colors.secondaryColor
+        }
+    }
+)
+
+
 const ShopNavigator = createDrawerNavigator(
-  {
-    Home: {
-      screen: HomeNavigator,
-      navigationOptions: {
-        drawerLabel: 'Home',
-        drawerIcon: <Ionicons name='ios-home'
-                              size={25}/>
-      }
+    {
+        Home: {
+            screen: HomeNavigator,
+            navigationOptions: {
+                drawerLabel: 'Home',
+                drawerIcon: <Ionicons name='ios-home'
+                                      size={25}/>
+            }
+        },
+        Profile: {
+            screen: ProfileNavigator,
+            navigationOptions: {
+                drawerLabel: 'My Profile',
+                drawerIcon: <Ionicons name='md-person'
+                                      size={25}/>
+            }
+        },
+        Cart: {
+            screen: CartNavigator,
+            navigationOptions: {
+                drawerLabel: 'Cart',
+                drawerIcon: <Ionicons name='md-albums'
+                                      size={25}/>
+            }
+        },
+        Orders: {
+            screen: OrdersNavigator,
+            navigationOptions: {
+                drawerLabel: 'My Orders',
+                drawerIcon: <Ionicons name='md-albums'
+                                      size={25}/>
+            }
+        },
+        Payment: {
+            screen: PaymentNavigator,
+            navigationOptions: {
+                drawerLabel: 'Payment Details',
+                drawerIcon: <Ionicons name='md-albums'
+                                      size={25}/>
+            }
+        },
+        PaymentCard: {
+            screen: PaymentCardNavigator,
+            navigationOptions: {
+                drawerLabel: 'Payment Details Card',
+                drawerIcon: <Ionicons name='md-albums'
+                                      size={25}/>
+            }
+        },
+        Feedback: {
+            screen: FeedbackNavigator,
+            navigationOptions: {
+                drawerLabel: 'Feedback',
+                drawerIcon: <Ionicons name='md-paper-plane'
+                                      size={25}/>
+            }
+        }
     },
-    Profile: {
-      screen: ProfileNavigator,
-      navigationOptions: {
-        drawerLabel: 'My Profile',
-        drawerIcon: <Ionicons name='md-person'
-                              size={25}/>
-      }
-    },
-    Cart: {
-      screen: CartNavigator,
-      navigationOptions: {
-        drawerLabel: 'Cart',
-        drawerIcon: <Ionicons name='md-albums'
-                              size={25}/>
-      }
-    },
-    Orders: {
-      screen: OrdersNavigator,
-      navigationOptions: {
-        drawerLabel: 'My Orders',
-        drawerIcon: <Ionicons name='md-albums'
-                              size={25}/>
-      }
-    },
-    Feedback: {
-      screen: FeedbackNavigator,
-      navigationOptions: {
-        drawerLabel: 'Feedback',
-        drawerIcon: <Ionicons name='md-paper-plane'
-                              size={25}/>
-      }
+    {
+        drawerBackgroundColor: Colors.secondaryColor,
+        contentOptions: {
+            activeTintColor: Colors.primaryColor
+        }
     }
-  },
-  {
-    drawerBackgroundColor: Colors.secondaryColor,
-    contentOptions: {
-      activeTintColor: Colors.primaryColor
-    }
-  }
 )
 
 const AuthNavigator = createStackNavigator(
-  {
-    Auth: AuthScreen,
-    ForgotPassword: ForgotPasswordScreen
-  },
-  {
-    headerMode: 'none',
-    navigationOptions: {
-      headerVisible: false
+    {
+        Auth: AuthScreen,
+        ForgotPassword: ForgotPasswordScreen
+    },
+    {
+        headerMode: 'none',
+        navigationOptions: {
+            headerVisible: false
+        }
     }
-  }
 )
 
 const MainNavigator = createSwitchNavigator({
-  Auth: AuthNavigator,
-  Shop: ShopNavigator
+    Auth: AuthNavigator,
+    Shop: ShopNavigator
 })
 
 export default createAppContainer(MainNavigator)
--- screens/delivery-details-screen.js
+++ screens/delivery-details-screen.js
@@ -1,15 +1,26 @@
 import React from 'react'
-import {StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native'
+import {StyleSheet, Text, TextInput, TouchableOpacity, View, Picker} from 'react-native'
 import {HeaderButtons, Item} from 'react-navigation-header-buttons'
 import HeaderButton from '../components/header-button'
 import {widthPercentageToDP as wp} from "react-native-responsive-screen";
+// import * as navigation from "react-navigation";
+// import Picker from "react-native-web";
 
-const DeliveryDetailsScreen = () => {
+const DeliveryDetailsScreen = (navigation) => {
     return (
         <View>
             <View style={styles.container}>
-                <Text >Personal Information </Text>
+                <Text style={styles.headerD}>Personal Information </Text>
             </View>
+            <View
+                style={{
+                    borderBottomColor: "#f07800",
+                    borderBottomWidth: 1,
+                    marginTop:16,
+                    width:"80%",
+                    alignSelf:"center"
+                }}
+            />
             <View >
                 <Text style={styles.textStyle} >Name </Text>
                 <TextInput
@@ -27,8 +38,17 @@ const DeliveryDetailsScreen = () => {
                 />
             </View>
             <View style={styles.container}>
-                <Text >Delivery Information </Text>
+                <Text style={styles.headerD}>Delivery Information </Text>
             </View>
+            <View
+                style={{
+                    borderBottomColor: "#f07800",
+                    borderBottomWidth: 1,
+                    marginTop:16,
+                    width:"80%",
+                    alignSelf:"center"
+                }}
+            />
             <View>
                 <Text style={styles.textStyle}>Street </Text>
                 <TextInput
@@ -40,11 +60,22 @@ const DeliveryDetailsScreen = () => {
                     placeholder="City"
                 />
                 <Text style={styles.textStyle}>Outlet </Text>
-                <TextInput
-                    style={styles.textInputStyle}
-                    placeholder="Outlet"
-                />
+                {/*<TextInput*/}
+                {/*    style={styles.textInputStyle}*/}
+                {/*    placeholder="Outlet"*/}
+                {/*/>*/}
+
             </View>
+            <Picker
+                style={styles.textInputStyles}
+                // style={{ height: 50, width: 150 }}
+                // onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+            >
+                <Picker.Item label="Colombo" value="java" />
+                <Picker.Item label="Malabe" value="js" />
+                <Picker.Item label="Gampaha" value="java1" />
+                <Picker.Item label="Jaffna" value="js2" />
+            </Picker>
             <View
                 style={{
                     marginTop: 20,
@@ -63,7 +94,7 @@ const DeliveryDetailsScreen = () => {
                         alignItems: "center",
                     }}
                     onPress={() => {
-                        // navigation.navigate("Shop");
+                        navigation.navigate('PaymentDetails')
                     }}
                 >
                     <Text style={{ color: "#fff", fontSize: 18 }}>CONTINUE</Text>
@@ -86,7 +117,7 @@ const styles = StyleSheet.create({
         width: wp("80%"),
         borderWidth: 1,
         borderRadius: 10,
-        height: 40,
+        height: 33,
         marginLeft: 35,
         // marginRight: 20,
         padding: 10,
@@ -95,11 +126,34 @@ const styles = StyleSheet.create({
     },
     textStyle: {
         width: wp("80%"),
-        height: 5,
+        // height: 5,
+        marginLeft: 35,
+        // marginRight: 20,
+        padding: 3,
+        marginTop: 4,
+        marginBottom:0,
+    },
+    headerD: {
+        fontWeight: 'bold',
+        // borderWidth: 1,
+        // borderColor: "#f07800",
+        // borderRadius: 10,
+        padding:5,
+        // marginBottom:20,
+        fontSize:20
+    },
+    textInputStyles: {
+        borderColor: "#f07800",
+        width: wp("80%"),
+        borderWidth: 1,
+        borderRadius: 10,
+        height: 40,
         marginLeft: 35,
         // marginRight: 20,
         padding: 10,
-        marginTop: 2,
+        marginTop: 8,
+        alignItems: "center",
+
     },
 });
 
